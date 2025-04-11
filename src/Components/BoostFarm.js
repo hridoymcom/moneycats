740846593e81db76533a604c1296311b9635c5d9
sugga-cloud <72293850+sugga-cloud@users.noreply.github.com>
2025-04-11 20:00:22 +0530

    .....

--- src/Components/BoostFarm.js
+++ src/Components/BoostFarm.js
@@ -244,10 +244,19 @@ className='w-full bg-cards py-4 px-4 rounded-[8px] flex justify-between items-ce
 
 <div className='w-[90px] h-[90px] fanbg border-[#616161] border-[4px] flex justify-center rounded-full items-center text-center relative'>
 <span className='w-[40px] h-[40px] fanbg border-[#616161] border-[1px] flex justify-center items-center rounded-full text-center relative transition-transform duration-300 hover:scale-110'>
-  <span className='absolute z-10 bg-[#3f2900] border-[1px] border-[#8b8b8b] rounded-full h-[24px] w-[24px] flex justify-center items-center'>
-  <img src='/animation.png' alt='dscfd' className='w-[24px] h-[24px]' />
-  </span>
-</span>
+<div className='w-[40px] h-[40px] rounded-full overflow-hidden'>
+  <img
+    src={isFarming ? '/farming.gif' : '/animation.png'}
+    alt='animation'
+    className='w-full h-full object-cover scale-150' // 👈 zoom effect
+    style={{ animationDuration: `${spinDuration}s` }}
+  />
+</div>
+
+            {/* <div className='absolute z-10 bg-[#3f2900] border-[1px] border-[#8b8b8b] rounded-full h-[34px] w-[34px] flex justify-center items-center'>
+              <img src='/dogs.webp' alt='sdfd' className='w-[14px]'/>
+            </div> */}
+          </span>
 
 
                     </div>
